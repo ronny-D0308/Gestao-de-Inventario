1a416ade5e152244c212652a970aa989d0b56a41
Ronny_Douglas <132111692+ronny-D0308@users.noreply.github.com>
2024-08-23 14:23:22 -0300

    ultima atualização phpAdicionar.php

--- phpAdicionar.php
+++ phpAdicionar.php
@@ -1,26 +1,18 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $bdname = "gestaodeInventario";
-
-    $conn = new mysqli( $servername, $username, $password, $bdname);
-
-    if($conn->connect_error) {
-        die("Erro na conexão". $conn->connect_error);
-    }
+    include('configPHP.php');
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome_produto = $_POST['nome_produto'];
         $quantidade = $_POST['quantidade'];
         $preco_produto = $_POST['preco'];
         $categoria = $_POST['categoria'];
+        $codigo_produto = $_POST['codigo_produto'];
 
         if(isset($_POST['data_venci'])) {
             $data_venci = $_POST['data_venci'];
         }
 
-        $sql = $conn -> prepare("INSERT INTO estoque (nome, quantidade, preco, data_venci, categoria) VALUES ('$nome_produto', '$quantidade', '$preco_produto', '$data_venci', '$categoria')");
+        $sql = $conn -> prepare("INSERT INTO produto (nome, quantidade, preco, data_venci, categoria, codigo_produto) VALUES ('$nome_produto', '$quantidade', '$preco_produto', '$data_venci', '$categoria', '$codigo_produto')");
         
         if($sql -> execute()) {
             echo "Produto adicionado com sucesso!!";
@@ -31,6 +23,6 @@
         $sql->close();
         $conn->close();
 
-        header('Location: index.php');
+        header('Location:index.php');
     }
 ?>
