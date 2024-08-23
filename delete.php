39ef2fc8e3c77e8f7d22e90f24aedefc44a67496
Ronny_Douglas <132111692+ronny-D0308@users.noreply.github.com>
2024-08-23 14:20:26 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,24 +3,14 @@
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $produto = $_POST['nome_produto'];
 
+    include('configPHP.php');
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $bdname = "gestaodeInventario";
-
-    $conn = new mysqli( $servername, $username, $password, $bdname);
-
-    if($conn->connect_error) {
-        die("Erro na conexão". $conn->connect_error);
-    }
-
-    $consul = "SELECT * FROM estoque WHERE nome LIKE '%$produto%' ";
+    $consul = "SELECT * FROM produto WHERE nome LIKE '%$produto%' ";
 
     $result = $conn->query($consul);
 
     if($result->num_rows > 0) {
-        $consulDelete = "DELETE FROM estoque WHERE nome LIKE '%$produto%'";
+        $consulDelete = "DELETE FROM produto WHERE nome LIKE '%$produto%'";
         $resultDelete = $conn->query($consulDelete);
     }
     header('Location: index.php');
